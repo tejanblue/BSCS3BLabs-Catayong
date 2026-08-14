f5d964654726d081e232deec1d63a5acacc3a5fc
tejanblue <banrasel7@gmail.com>
2026-08-14 12:12:18 +0800

    added the code for page2

---
+++ lab3/page2.php
@@ -0,0 +1,20 @@
+<?php
+    $title = "My Team";
+    $header = "The Team That Competes With Me";
+?>
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+    </head>
+    <body>
+        <h1><?php echo $header; ?></h1>
+        <p>This page is dedicated to my team, the group of people I stand shoulder to shoulder with during competitions. We prepare together, push through challenges together, and celebrate every win as one team. Below are some snapshots of us in action.</p>
+        <img src="Images/SyntxxTeam.jpg" alt="My team photo" width="400">
+        <img src="Images/Competition.jpg" alt="Our team during a competition" width="400">
+        <p>
+            <a href="page1.php">Go to About Me Page</a>
+            |
+            <a href="page3.php">Go to My Friend Group Page</a>
+        </p>
+    </body>
+</html>
