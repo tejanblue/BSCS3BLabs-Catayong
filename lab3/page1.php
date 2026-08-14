f8259e76b3ff4ad1ec1896fece528c3aee77963e
tejanblue <banrasel7@gmail.com>
2026-08-14 12:01:35 +0800

    starts a code

---
+++ lab3/page1.php
@@ -0,0 +1,19 @@
+<?php
+    $title = "About Me";
+    $header = "Hello, I'm the Face Behind This Portfolio";
+?>
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+    </head>
+    <body>
+        <h1><?php echo $header; ?></h1>
+        <p>Welcome to my personal corner of the web. I am a student who enjoys exploring technology, learning new skills, and taking on challenges both inside and outside the classroom. This page gives a quick glimpse of who I am beyond the code and the coursework.</p>
+        <img src="Images/Me.jpg" alt="A photo of me" width="400">
+        <p>
+            <a href="page2.php">Go to My Team Page</a>
+            |
+            <a href="page3.php">Go to My Friend Group Page</a>
+        </p>
+    </body>
+</html>
