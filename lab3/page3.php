a2364208f6b7f1b809f0bb206f33a7114abdb3ea
tejanblue <banrasel7@gmail.com>
2026-08-14 12:15:04 +0800

    code for page3

---
+++ lab3/page3.php
@@ -0,0 +1,20 @@
+<?php
+    $title = "My Friend Group";
+    $header = "The Friends Who Make College Fun";
+?>
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+    </head>
+    <body>
+        <h1><?php echo $header; ?></h1>
+        <p>Outside of academics and competitions, this page is all about my friend group. These are the people I laugh with, learn with, and create memories with since our first year. They make the everyday moments worth looking back on.</p>
+        <img src="Images/1styearFriendGroup1.jpg" alt="My friend group photo one" width="400">
+        <img src="Images/1styearFriendGroup2.jpg" alt="My friend group photo two" width="400">
+        <p>
+            <a href="page1.php">Go to About Me Page</a>
+            |
+            <a href="page2.php">Go to My Team Page</a>
+        </p>
+    </body>
+</html>
